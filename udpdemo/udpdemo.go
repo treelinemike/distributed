912af131bf0ca1ca0b41d9b1c3a8df0f7851a920
Mike Kokko <mike.kokko@gmail.com>
2024-09-26 09:11:45 -0400

    blink LEDs on remote arduinos

--- udpdemo/udpdemo.go
+++ udpdemo/udpdemo.go
@@ -6,11 +6,20 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/exec"
+	"time"
 )
 
 func main() {
 	log.Println("Starting program")
-	serverAddr := "192.168.88.118:2390"
+
+	// see if we got an IP as a command line argument
+	serverAddr := "192.168.88.201:2390"
+	if len(os.Args) == 2 {
+		serverAddr = os.Args[1] + ":2390"
+	}
+
 	/*udpAddr, err := net.ResolveUDPAddr("udp",serverAddr)
 	if err != nil{
 		log.Fatal("Failed to resolve UDP Address: ",err.Error())
@@ -21,19 +30,41 @@ func main() {
 		log.Fatal("Failed to dial UDP Address: ", err.Error())
 	}
 
-	_, err = udpnet.Write([]byte("Hello, world!"))
-	if err != nil {
-		log.Fatal("Failed to write to UDP: ", err.Error())
-	}
+	lightstatus := false
+	for {
+		if !lightstatus {
+			_, err = udpnet.Write([]byte("lighton"))
+			lightstatus = true
 
-	// Read from the connection untill a new line is send
-	data, err := bufio.NewReader(udpnet).ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+			_, err := exec.Command("blink1-on.sh").Output()
+			if err != nil {
+				log.Println("Could not turn on blink(1) indicator")
+			}
 
-	// Print the data read from the connection to the terminal
-	log.Print("Received: ", string(data))
+		} else {
+			_, err = udpnet.Write([]byte("lightoff"))
+			lightstatus = false
+
+			_, err := exec.Command("blink1-off.sh").Output()
+			if err != nil {
+				log.Println("Could not turn on blink(1) indicator")
+			}
+
+		}
+		if err != nil {
+			log.Fatal("Failed to write to UDP: ", err.Error())
+		}
+
+		// Read from the connection untill a new line is send
+		data, err := bufio.NewReader(udpnet).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// Print the data read from the connection to the terminal
+		log.Print("Received: ", string(data))
+		time.Sleep(time.Second * 1)
+	}
 
 }
