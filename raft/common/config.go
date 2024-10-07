8b47f56243d4bca95c39f3cf42bb2bf651eeb5f4
Mike Kokko <mike.kokko@gmail.com>
2024-10-06 23:30:27 -0400

    slowly working on raft

--- raft/common/config.go
+++ raft/common/config.go
@@ -13,13 +13,18 @@ type (
 		Address string `mapstructure:"Address"`
 		Port    string `mapstructure:"Port"`
 	}
-	ServerConfig struct {
+	Timeout struct {
+		Min_ms int `mapstructure:"Min_ms"`
+		Max_ms int `mapstructure:"Max_ms"`
+	}
+	RaftConfig struct {
 		Servers map[string]NetworkAddress `mapstructure:"Servers"`
+		Timeout Timeout                   `mapstructure:"Timeout"`
 	}
 )
 
 // see: https://betterprogramming.pub/parsing-and-creating-yaml-in-go-crash-course-2ec10b7db850
-func LoadRaftConfig(configFileName string, Servers map[string]NetworkAddress) error {
+func LoadRaftConfig(configFileName string, Servers map[string]NetworkAddress, t *Timeout) error {
 
 	configFile, err := os.ReadFile(configFileName)
 	if err != nil {
@@ -33,16 +38,23 @@ func LoadRaftConfig(configFileName string, Servers map[string]NetworkAddress) er
 		log.Fatal(err)
 	}
 
-	var cfg ServerConfig
-
+	// decode config file into mapstructure
+	var cfg RaftConfig
 	decoder, _ := mapstructure.NewDecoder(&mapstructure.DecoderConfig{WeaklyTypedInput: true, Result: &cfg})
 	err = decoder.Decode(yamlIn)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// store server list
 	for key := range cfg.Servers {
 		Servers[key] = cfg.Servers[key]
 	}
 
+	// store timeout
+	t.Min_ms = cfg.Timeout.Min_ms
+	t.Max_ms = cfg.Timeout.Max_ms
+
+	// done
 	return nil
 }
--- raft/server/server.go
+++ raft/server/server.go
@@ -4,10 +4,50 @@ import (
 	"engg415/raft/common"
 	"fmt"
 	"log"
+	"math/rand/v2"
+	"net"
+	"net/http"
+	"net/rpc"
 	"os"
+	"time"
 )
 
+type RaftAPI int
+
+// TODO: struct members don't need to be exported if we're keeping in the server.go main package
+type AEParams struct {
+	term         int
+	leaderId     int
+	prevLogIndex int
+	leaderCommit int
+}
+
+func (r *RaftAPI) AppendEntries(p AEParams, resp *int) error {
+	fmt.Printf("AppendEntriesRPC: term %d\n", p.term)
+	return nil
+}
+
+type RVParams struct {
+	term         int
+	candidateId  int
+	lastLogIndex int
+	lastLogTerm  int
+}
+
+type RVResp struct {
+	term        int
+	voteGranted bool
+}
+
+func (r *RaftAPI) RequestVote(p RVParams, resp *RVResp) error {
+	fmt.Printf("RequestVoteRPC: term %d\n", p.term)
+	return nil
+}
+
 func main() {
+
+	// format log
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 
 	// load cluster configuration
 	if len(os.Args) != 3 {
@@ -17,11 +57,41 @@ func main() {
 	filename := os.Args[1]
 	selfkey := os.Args[2]
 	servers := make(map[string]common.NetworkAddress)
-	common.LoadRaftConfig(filename, servers)
+
+	// load config
+	log.Println("Loading Raft config file")
+	var t common.Timeout
+	common.LoadRaftConfig(filename, servers, &t)
+	fmt.Printf("Timeout range: [%d, %d]\n", t.Min_ms, t.Max_ms)
+	fmt.Printf("Random draw: %d\n", rand.IntN(t.Max_ms-t.Min_ms)+t.Min_ms) // test picking a random timeout value
 
 	// make sure provided selfkey is in map from config file
 	_, ok := servers[selfkey]
 	if !ok {
 		log.Fatal("Key ", selfkey, " is not in cluster config file")
 	}
+
+	// serve RaftAPI
+	log.Println("Registering server API for access on port", servers[selfkey].Port)
+	thisapi := new(RaftAPI)
+	rpc.Register(thisapi)
+	rpc.HandleHTTP()
+	l, err := net.Listen("tcp", ":"+servers[selfkey].Port)
+	if err != nil {
+		log.Fatal("Error listening - is the server already running?")
+		return
+	}
+	go http.Serve(l, nil)
+	log.Println("Ready to play")
+
+	// start a timer
+	i := 0
+	timer1 := time.NewTimer(2 * time.Second)
+	for {
+		<-timer1.C
+		fmt.Printf("%d: calling election!\n", i)
+		i++
+		timer1.Reset(2 * time.Second)
+	}
+
 }
