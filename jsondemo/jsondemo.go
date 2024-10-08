6f73b8a8d0568fe7e7184de93565eb351becd1dd
Mike Kokko <mike.kokko@gmail.com>
2024-10-08 17:15:19 -0400

    working on json test

--- jsondemo/jsondemo.go
+++ jsondemo/jsondemo.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -15,17 +16,36 @@ type nvstate struct {
 var st nvstate
 
 func main() {
+
+	// name of the json file to which we're storing our nonvolatile data
+	jsonfilename := "nvstate.json"
+
+	// ensure that we're starting from scratch with no json file in local directory
+	filestat, err := os.Stat(jsonfilename)
+	if err != nil {
+		log.Println("File ", jsonfilename, " does not exist...")
+	} else {
+		if filestat.IsDir() {
+			log.Fatal("Error: ", jsonfilename, " is a directory!")
+		}
+		log.Println("File ", jsonfilename, " alread exists, deleting it...")
+		err := os.Remove(jsonfilename)
+		if err != nil {
+			log.Fatal("Could not remove file")
+		}
+	}
+
+	// now create an object and write to a json file
 	st.Term = 12
 	st.LeaderID = "Server1"
 	st.Log = append(st.Log, "first command", "second command")
-
-	outfile, _ := os.OpenFile("nvstate.json", os.O_CREATE, os.ModePerm)
+	outfile, _ := os.OpenFile(jsonfilename, os.O_CREATE, os.ModePerm)
 	encoder := json.NewEncoder(outfile)
 	encoder.SetIndent("", "  ")
 	encoder.Encode(st)
 	outfile.Close()
 
-	infile, _ := os.OpenFile("nvstate.json", os.O_RDONLY, os.ModePerm)
+	infile, _ := os.OpenFile(jsonfilename, os.O_RDONLY, os.ModePerm)
 	decoder := json.NewDecoder(infile)
 	var readnv nvstate
 	decoder.Decode(&readnv)
