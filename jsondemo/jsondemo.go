802aaa158d52c32dd1a4357a3e8dc4611aa66e67
Mike Kokko <mike.kokko@gmail.com>
2024-10-08 21:53:23 -0400

    better json test

--- jsondemo/jsondemo.go
+++ jsondemo/jsondemo.go
@@ -22,35 +22,35 @@ func main() {
 
 	// ensure that we're starting from scratch with no json file in local directory
 	filestat, err := os.Stat(jsonfilename)
-	if err != nil {
-		log.Println("File ", jsonfilename, " does not exist...")
-	} else {
+	if err == nil {
+		// make sure this path isn't a directory
 		if filestat.IsDir() {
 			log.Fatal("Error: ", jsonfilename, " is a directory!")
 		}
-		log.Println("File ", jsonfilename, " alread exists, deleting it...")
-		err := os.Remove(jsonfilename)
-		if err != nil {
-			log.Fatal("Could not remove file")
-		}
+
+		// load json file into nv struct
+		log.Println("File ", jsonfilename, " exists, loading it...")
+		infile, _ := os.OpenFile(jsonfilename, os.O_RDONLY, os.ModePerm)
+		decoder := json.NewDecoder(infile)
+		decoder.Decode(&st)
+		infile.Close()
+
+	} else {
+		log.Println("File ", jsonfilename, " does not exist, so initialize nv struct...")
+		st.Term = 12
+		st.LeaderID = "Server1"
+		st.Log = append(st.Log, "first command", "second command")
 	}
 
+	// show nv struct
+	fmt.Println("NV struct contents: ", st)
+
 	// now create an object and write to a json file
-	st.Term = 12
-	st.LeaderID = "Server1"
-	st.Log = append(st.Log, "first command", "second command")
+	log.Println("Writing nv struct to file...")
 	outfile, _ := os.OpenFile(jsonfilename, os.O_CREATE, os.ModePerm)
 	encoder := json.NewEncoder(outfile)
 	encoder.SetIndent("", "  ")
 	encoder.Encode(st)
 	outfile.Close()
-
-	infile, _ := os.OpenFile(jsonfilename, os.O_RDONLY, os.ModePerm)
-	decoder := json.NewDecoder(infile)
-	var readnv nvstate
-	decoder.Decode(&readnv)
-	infile.Close()
-
-	fmt.Println("Read from json: ", readnv)
 
 }
