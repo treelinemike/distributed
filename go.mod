d12db0f2afbb239e761da624b94664232f4393d2
Mike Kokko <mike.kokko@gmail.com>
2024-12-16 06:08:10 -0500

    working on mazeviz

--- go.mod
+++ go.mod
@@ -1,10 +1,22 @@
 module engg415
 
-go 1.18
+go 1.22.0
+
+toolchain go1.23.4
 
 require (
+	github.com/hajimehoshi/ebiten/v2 v2.8.5
 	github.com/mitchellh/mapstructure v1.5.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
-require github.com/go-echarts/go-echarts/v2 v2.4.2 // indirect
+require (
+	github.com/ebitengine/gomobile v0.0.0-20240911145611-4856209ac325 // indirect
+	github.com/ebitengine/hideconsole v1.0.0 // indirect
+	github.com/ebitengine/purego v0.8.0 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
--- mazeviz/mazeviz.go
+++ mazeviz/mazeviz.go
@@ -1,1 +1,41 @@
-import ()
+package main
+
+import (
+	"image/color"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+	for cols := 0; cols < 16; cols++ {
+		for rows := 0; rows < 16; rows++ {
+			cell := ebiten.NewImage(50, 50)
+			cell.Fill(color.RGBA{0xc0, 0x00, 0xc0, 0xff})
+			drawopts := new(ebiten.DrawImageOptions)
+			drawopts.GeoM.Translate(float64(2+52*cols), float64(2+52*rows))
+			screen.DrawImage(cell, drawopts)
+		}
+	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	s := ebiten.Monitor().DeviceScaleFactor()
+	return int(float64(outsideWidth) * s), int(float64(outsideHeight) * s)
+}
+
+func main() {
+	ebiten.SetWindowSize(834, 834)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
