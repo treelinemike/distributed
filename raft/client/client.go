1246443141fa5c6d40dc7dfbaf26adef9ef86da7
Mike Kokko <michael.kokko@dartmouth.edu>
2025-12-29 06:32:02 -0500

    started working on client to read word list

---
+++ raft/client/client.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand/v2"
+	"os"
+	"slices"
+)
+
+func main() {
+
+	// open the text file
+	file, err := os.OpenFile("google-10000-english-usa-no-swears.txt", os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		log.Fatalf("Error reading file: %v", err)
+	}
+	defer file.Close()
+
+	// load each line of file into an element of a slice
+	var strings []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		strings = append(strings, scanner.Text())
+	}
+
+	// seed the RNG so we can reproduce results
+	source := rand.New(rand.NewPCG(102, 9945))
+	rng := rand.New(source)
+
+	// select random strings from the slice to store in the raft cluster
+	for range 10 {
+		idx := rng.IntN(len(strings))
+		fmt.Printf("%d (of %d): %s\n", idx, len(strings), strings[idx])
+		strings = slices.Delete(strings, idx, idx+1)
+	}
+	fmt.Printf("Read %d strings from file\n", len(strings))
+	fmt.Printf("%d: %s\n", 0, strings[0])
+
+}
